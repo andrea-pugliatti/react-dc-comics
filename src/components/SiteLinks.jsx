1c8510be5ec32e8a82f7300226a343f5eb4a6903
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-04 16:39:09 +0100

    Divide footer in two components

---
+++ src/components/SiteLinks.jsx
@@ -0,0 +1,105 @@
+export default function SiteLinks() {
+  return (
+    <div className="site-links">
+      <div className="logo container">
+        <div className="row">
+          <div>
+            <h3>DC Comics</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Shop</h3>
+            <ul>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>DC</h3>
+            <ul>
+              <li>
+                <a href="#">Terms Of Use</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Ad Choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscriptions</a>
+              </li>
+              <li>
+                <a href="#">Talent Workshops</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Sites</h3>
+            <ul>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Socials.jsx
@@ -0,0 +1,29 @@
+export default function Socials() {
+  return (
+    <div className="socials">
+      <div className="container d-flex space-between">
+        <div className="signup">
+          <a href="#">Sign-Up Now!</a>
+        </div>
+        <div className="social-links">
+          <span>Follow Us</span>
+          <a href="#">
+            <img src="./src/assets/img/footer-facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="./src/assets/img/footer-twitter.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="./src/assets/img/footer-youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="./src/assets/img/footer-pinterest.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="./src/assets/img/footer-periscope.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
