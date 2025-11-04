322831e570254b7f9cbcf6540d010e40c2d0d90e
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-04 16:09:09 +0100

    Bonus: add banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,30 @@
+export default function Banner() {
+  return (
+    <>
+      <div className="banner">
+        <div className="container d-flex space-between">
+          <div className="banner-element">
+            <img src="./src/assets/img/buy-comics-digital-comics.png" />
+            <span>Digital Comics</span>
+          </div>
+          <div className="banner-element">
+            <img src="./src/assets/img/buy-comics-merchandise.png" />
+            <span>DC Merchandise</span>
+          </div>
+          <div className="banner-element">
+            <img src="./src/assets/img/buy-comics-subscriptions.png" />
+            <span>Subscription</span>
+          </div>
+          <div className="banner-element">
+            <img src="./src/assets/img/buy-comics-shop-locator.png" />
+            <span>Comic Shop Locator</span>
+          </div>
+          <div className="banner-element">
+            <img src="./src/assets/img/buy-dc-power-visa.svg" />
+            <span>DC Power Visa</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import Banner from "./Banner";
+
 export default function Main() {
   return (
     <main>
@@ -6,30 +8,7 @@ export default function Main() {
           {"-->"} Content goes here {"<--"}
         </div>
       </div>
-      <div className="banner">
-        <div className="container d-flex space-between">
-          <div className="banner-element">
-            <img src="./src/assets/img/buy-comics-digital-comics.png" />
-            <span>Digital Comics</span>
-          </div>
-          <div className="banner-element">
-            <img src="./src/assets/img/buy-comics-merchandise.png" />
-            <span>DC Merchandise</span>
-          </div>
-          <div className="banner-element">
-            <img src="./src/assets/img/buy-comics-subscriptions.png" />
-            <span>Subscription</span>
-          </div>
-          <div className="banner-element">
-            <img src="./src/assets/img/buy-comics-shop-locator.png" />
-            <span>Comic Shop Locator</span>
-          </div>
-          <div className="banner-element">
-            <img src="./src/assets/img/buy-dc-power-visa.svg" />
-            <span>DC Power Visa</span>
-          </div>
-        </div>
-      </div>
+      <Banner />
     </main>
   );
 }
