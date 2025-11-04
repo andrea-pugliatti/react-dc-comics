e9e1ee8e6e43568bfbe729281bd527cf2c96c75f
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-04 14:53:44 +0100

    Add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,42 @@
 export default function Header() {
-  return <header></header>;
+  const logoPath = "./src/assets/img/dc-logo.png";
+  return (
+    <header>
+      <div className="container d-flex space-between">
+        <img src={logoPath} alt="" />
+        <ul>
+          <li>
+            <a href="#">Characters</a>
+          </li>
+          <li>
+            <a href="#">Comics</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+          <li>
+            <a href="#">Collectibles</a>
+          </li>
+          <li>
+            <a href="#">Videos</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
