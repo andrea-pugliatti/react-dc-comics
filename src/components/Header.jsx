d2daf892aa017c04206b6f3af4711f79f664983e
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-05 15:35:18 +0100

    Rethink the navbar data structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 export default function Header() {
   const logoPath = "./src/assets/img/dc-logo.png";
+
   const navbar = [
-    "Characters",
-    "Comics",
-    "Movies",
-    "TV",
-    "Games",
-    "Collectibles",
-    "Videos",
-    "Fans",
-    "News",
-    "Shop",
+    { id: 1, name: "Characters", link: "#", isActive: false },
+    { id: 2, name: "Comics", link: "#", isActive: true },
+    { id: 3, name: "Movies", link: "#", isActive: false },
+    { id: 4, name: "TV", link: "#", isActive: false },
+    { id: 5, name: "Games", link: "#", isActive: false },
+    { id: 6, name: "Collectibles", link: "#", isActive: false },
+    { id: 7, name: "Videos", link: "#", isActive: false },
+    { id: 8, name: "Fans", link: "#", isActive: false },
+    { id: 9, name: "News", link: "#", isActive: false },
+    { id: 10, name: "Shop", link: "#", isActive: false },
   ];
 
   return (
@@ -20,9 +21,9 @@ export default function Header() {
           <img src={logoPath} />
         </a>
         <ul>
-          {navbar.map((current, index) => (
-            <li key={index}>
-              <a href="#">{current}</a>
+          {navbar.map((current) => (
+            <li key={current.id} className={current.isActive ? "active" : ""}>
+              <a href={current.link}>{current.name}</a>
             </li>
           ))}
         </ul>
