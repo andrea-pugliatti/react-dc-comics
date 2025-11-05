0eca4b1cb5ce46cc341c7b793c32767c1dfba6bc
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-05 22:43:39 +0100

    Change data structure for Socials component

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,27 +1,28 @@
 export default function Socials() {
-  const imageLinks = [
-    "./src/assets/img/footer-facebook.png",
-    "./src/assets/img/footer-twitter.png",
-    "./src/assets/img/footer-youtube.png",
-    "./src/assets/img/footer-pinterest.png",
-    "./src/assets/img/footer-periscope.png",
-  ];
+	const links = [
+		{ id: 1, img: "./src/assets/img/footer-facebook.png", link: "#" },
+		{ id: 2, img: "./src/assets/img/footer-twitter.png", link: "#" },
+		{ id: 3, img: "./src/assets/img/footer-youtube.png", link: "#" },
+		{ id: 4, img: "./src/assets/img/footer-pinterest.png", link: "#" },
+		{ id: 5, img: "./src/assets/img/footer-periscope.png", link: "#" },
+	];
+	const signupLink = "#";
 
-  return (
-    <div className="socials">
-      <div className="container d-flex space-between">
-        <div className="signup">
-          <a href="#">Sign-Up Now!</a>
-        </div>
-        <div className="social-links">
-          <span>Follow Us</span>
-          {imageLinks.map((current, index) => (
-            <a key={index} href="#">
-              <img src={current} />
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="socials">
+			<div className="container d-flex space-between">
+				<div className="signup">
+					<a href={signupLink}>Sign-Up Now!</a>
+				</div>
+				<div className="social-links">
+					<span>Follow Us</span>
+					{links.map((current) => (
+						<a key={current.id} href={current.link}>
+							<img src={current.img} alt="" />
+						</a>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
