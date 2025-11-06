c2cfba052b0b4dbda8a676b1c318f3ca7bc4f3b5
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-06 11:57:03 +0100

    Move the data from Comics and Banner into App

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,37 +1,4 @@
-export default function Banner() {
-	const banner = [
-		{
-			id: 1,
-			title: "Digital Comics",
-			link: "#",
-			imageSrc: "./src/assets/img/buy-comics-digital-comics.png",
-		},
-		{
-			id: 2,
-			title: "DC Merchandise",
-			link: "#",
-			imageSrc: "./src/assets/img/buy-comics-merchandise.png",
-		},
-		{
-			id: 3,
-			title: "Subscription",
-			link: "#",
-			imageSrc: "./src/assets/img/buy-comics-subscriptions.png",
-		},
-		{
-			id: 4,
-			title: "Comic Shop Locator",
-			link: "#",
-			imageSrc: "./src/assets/img/buy-comics-shop-locator.png",
-		},
-		{
-			id: 5,
-			title: "DC Power Visa",
-			link: "#",
-			imageSrc: "./src/assets/img/buy-dc-power-visa.svg",
-		},
-	];
-
+export default function Banner({ banner }) {
 	return (
 		<div className="banner">
 			<div className="container d-flex space-between">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,18 @@
 import Banner from "./Banner";
 import Comics from "./Comics";
 
-export default function Main() {
+export default function Main({ comics, banner }) {
 	return (
 		<main>
 			<div className="jumbotron"></div>
 
 			<div className="content">
 				<div className="container">
-					<Comics />
+					<Comics comics={comics} />
 				</div>
 			</div>
 
-			<Banner />
+			<Banner banner={banner} />
 		</main>
 	);
 }
