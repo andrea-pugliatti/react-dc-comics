8368f9fc3b631d466bbbdcf6d0ee61bbeeb0a568
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-06 11:52:06 +0100

    Move the data from Header in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+	const logoPath = "./src/assets/img/dc-logo.png";
+
+	const navbar = [
+		{ id: 1, name: "Characters", link: "#", isActive: false },
+		{ id: 2, name: "Comics", link: "#", isActive: true },
+		{ id: 3, name: "Movies", link: "#", isActive: false },
+		{ id: 4, name: "TV", link: "#", isActive: false },
+		{ id: 5, name: "Games", link: "#", isActive: false },
+		{ id: 6, name: "Collectibles", link: "#", isActive: false },
+		{ id: 7, name: "Videos", link: "#", isActive: false },
+		{ id: 8, name: "Fans", link: "#", isActive: false },
+		{ id: 9, name: "News", link: "#", isActive: false },
+		{ id: 10, name: "Shop", link: "#", isActive: false },
+	];
+
+	return (
+		<>
+			<Header menu={navbar} logo={logoPath} />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,12 @@
-export default function Header() {
-	const logoPath = "./src/assets/img/dc-logo.png";
-
-	const navbar = [
-		{ id: 1, name: "Characters", link: "#", isActive: false },
-		{ id: 2, name: "Comics", link: "#", isActive: true },
-		{ id: 3, name: "Movies", link: "#", isActive: false },
-		{ id: 4, name: "TV", link: "#", isActive: false },
-		{ id: 5, name: "Games", link: "#", isActive: false },
-		{ id: 6, name: "Collectibles", link: "#", isActive: false },
-		{ id: 7, name: "Videos", link: "#", isActive: false },
-		{ id: 8, name: "Fans", link: "#", isActive: false },
-		{ id: 9, name: "News", link: "#", isActive: false },
-		{ id: 10, name: "Shop", link: "#", isActive: false },
-	];
-
+export default function Header({ menu, logo }) {
 	return (
 		<header>
 			<div className="container d-flex space-between">
 				<a href="/">
-					<img src={logoPath} alt="" />
+					<img src={logo} alt="" />
 				</a>
 				<ul>
-					{navbar.map((current) => (
+					{menu.map((current) => (
 						<li key={current.id} className={current.isActive ? "active" : ""}>
 							<a href={current.link}>{current.name}</a>
 						</li>
